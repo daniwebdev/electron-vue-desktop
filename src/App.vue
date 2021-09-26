4951b3231b954bda45118cab304c8a1ad002fe14
Dani <dani.webdev@gmail.com>
2021-09-26 15:27:05 +0700

    update view

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <div id="topbar"></div>
+    <div id="sidebar">
+      <div class="menu-item">
+        <svg
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+          stroke="currentColor"
+          stroke-width="2"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="css-i6dzq1"
+        >
+          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+          <polyline points="9 22 9 12 15 12 15 22"></polyline>
+        </svg>
+      </div>
+    </div>
+
+    <div id="bottombar">
+      <div class="bottom-left"></div>
+      <div class="bottom-right">
+        <span class="item date"> 00:00:00 </span>
+      </div>
+    </div>
+    <div id="content">
+      <router-view></router-view>
+    </div>
   </div>
-  <router-view />
 </template>
 
+<script>
+import moment from "moment";
+
+export default {
+  mounted: () => {
+    this.$router.push("/");
+
+    console.log("test");
+
+    setInterval(() => {
+      document.querySelector(".date").innerHTML = moment().format("HH:mm:ss");
+    }, 1000);
+  },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url("./assets/default.scss");
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
+// import "./registerServiceWorker";
 import router from "./router";
 
 createApp(App).use(router).mount("#app");
+
+// export default new Vue({
+//   router,
+//   render: (h) => h(App),
+//   mounted: () => {
+//     // Prevent white screen when the app is launched (in production build)
+//   },
+// }).$mount("#app");
